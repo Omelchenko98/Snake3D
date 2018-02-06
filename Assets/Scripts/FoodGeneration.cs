907942217afefc8f636ab15b4135bc09f5b2d5c2
Classic_1 <1ki15b.omelchenko@gmail.com>
2018-02-06 16:27:38 +0200

    Create Game Ground and all  other components

--- Assets/Scripts/FoodGeneration.cs
+++ Assets/Scripts/FoodGeneration.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Snake : MonoBehaviour {
+public class FoodGeneration : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
