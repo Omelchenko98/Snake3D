0d2000f69f2c8b2606fd0f2324ac3a0d1a28f9e5
Classic_1 <1ki15b.omelchenko@gmail.com>
2018-02-07 05:51:20 +0200

    Random Food Generation

--- Assets/Scripts/FoodGeneration.cs
+++ Assets/Scripts/FoodGeneration.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodGeneration : MonoBehaviour {
+    public float XSize = 9f;
+    public float ZSize = -19f;
+
+    public GameObject foodPrefab;
+
+    public Vector3 curPos;
+
+    public GameObject curFood;
+
 
 	// Use this for initialization
 	void Start () {
+        RandomPos();
+        curFood = GameObject.Instantiate(foodPrefab, curPos, Quaternion.identity) as GameObject;
 		
 	}
 	
+    void RandomPos()
+    {
+        curPos = new Vector3(Random.Range(XSize, -XSize), 12.16f, Random.Range(ZSize, -1f));
+    }
 	// Update is called once per frame
 	void Update () {
 		
