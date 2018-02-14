529270b49708b2d35b3c5ff7b3ee8a1fd0519d36
Classic_1 <1ki15b.omelchenko@gmail.com>
2018-02-14 10:47:08 +0200

    Ready Game but have bugg with  Light so need fixed

--- Assets/Scripts/FoodGeneration.cs
+++ Assets/Scripts/FoodGeneration.cs
@@ -1,31 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class FoodGeneration : MonoBehaviour {
-    public float XSize = 9f;
-    public float ZSize = -19f;
+public class FoodGeneration : MonoBehaviour
+{
 
+    public float XSize = 8.8f;
+    public float ZSize = 8.8f;
     public GameObject foodPrefab;
-
     public Vector3 curPos;
-
     public GameObject curFood;
-
-
-	// Use this for initialization
-	void Start () {
+    void AddNewFood()
+    {
         RandomPos();
         curFood = GameObject.Instantiate(foodPrefab, curPos, Quaternion.identity) as GameObject;
-		
-	}
-	
+    }
     void RandomPos()
     {
-        curPos = new Vector3(Random.Range(XSize, -XSize), 12.16f, Random.Range(ZSize, -1f));
+        curPos = new Vector3(Random.Range(XSize * -1, XSize), 0.25f, Random.Range(ZSize * -1, ZSize));
     }
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+        if (!curFood)
+        {
+            AddNewFood();
+        }
+        else
+        {
+            return;
+        }
+    }
 }
---
+++ Assets/Scripts/Walls.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walls : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("SnakeMain"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+    }
+}
