e276e3ac43bdd0a3d2e46ebce323a84450c0bced
Warder <b.romesh@yandex.ru>
2018-02-20 13:38:48 +0200

    Comments
    Storage more info in simple c# class

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Firstly I thinks we have no nesesary to inherit from MonoBehaviour
+// Better to create simple class Food, where storage all data about it, position, point etc.
+// But anyway we need to have a FoodView class, which would display this info at game scene.
+// I'll explain this principle at the lesson
 public class Food : MonoBehaviour
 {
+
+    // Look in snakeMovement class
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("SnakeMain"))
