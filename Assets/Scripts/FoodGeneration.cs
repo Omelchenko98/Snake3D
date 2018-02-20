ed360f1482164a0d41e2297681bbe30983ebc433
Warder <b.romesh@yandex.ru>
2018-02-20 13:34:23 +0200

    Comments
    Magic numbers, access to variables

--- Assets/Scripts/FoodGeneration.cs
+++ Assets/Scripts/FoodGeneration.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class FoodGeneration : MonoBehaviour
 {
-
+    // Doesn't good idea to keep it public
+    // Better to create property(Read about it) or get/set functions for change it 
     public float XSize = 8.8f;
     public float ZSize = 8.8f;
     public GameObject foodPrefab;
     public Vector3 curPos;
     public GameObject curFood;
+    
+    // Good enough, but can be better if we would have a posibility to create different food
     void AddNewFood()
     {
         RandomPos();
@@ -16,9 +19,18 @@ public class FoodGeneration : MonoBehaviour
     }
     void RandomPos()
     {
+        // XSize*-1 == -Xsize;
+        // 0.25f magic number; create a variable
+        // and Random.Range, better to store it return value to local variable and use after it
         curPos = new Vector3(Random.Range(XSize * -1, XSize), 0.25f, Random.Range(ZSize * -1, ZSize));
     }
 
+    // For current version good
+    // But for this issue "else { return;}" unnesesary
+    // and you can use principle of early return
+    // if(currentFood != null) return;
+    //
+    // AddNewFood();
     void Update()
     {
         if (!curFood)
